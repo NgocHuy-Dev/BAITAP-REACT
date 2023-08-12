e8cb5c6a5ef64d049e01bdc9440c6cde9c18d25c
Ngoc Huy <hongochuy2640@gmail.com>
2023-08-12 17:23:36 +0700

    .

--- src/BaiTapLayoutComponent/Footer.jsx
+++ src/BaiTapLayoutComponent/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <div className="text-center bg-dark p-5 text-white">
+    <div className="text-center bg-dark p-5 text-white mt-3">
       Coppyright ©️ Your Website 2023
     </div>
   );
