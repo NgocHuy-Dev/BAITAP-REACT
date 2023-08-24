92fc8e14bfece72211aebc3355e5e8aedbf69651
Ngoc Huy <hongochuy2640@gmail.com>
2023-08-24 23:29:00 +0700

    .

--- src/BaiTapReactBuoi2-Glasses/ModalOverlay.jsx
+++ src/BaiTapReactBuoi2-Glasses/ModalOverlay.jsx
@@ -1,28 +1,28 @@
 import React from "react";
 
 export default function ModalOverlay({ product }) {
+  // console.log("data", data);
+  // console.log("ID in Overlay:", productID);
+  // let glasses = data[productID];
+  // console.log(glasses);
   return (
     <div className="overlay">
-      {product.map((glasses) => {
-        return (
-          <div className="overlay-content">
-            <div className="overlay-img">
-              <img
-                src={glasses.url}
-                alt=""
-                width="170px"
-                height="60px"
-                className="object-fit-cover"
-              />
-            </div>
+      <div className="overlay-content">
+        <div className="overlay-img">
+          <img
+            src={product.url}
+            alt=""
+            width="170px"
+            height="60px"
+            className="object-fit-cover"
+          />
+        </div>
 
-            <div className="overlay-text">
-              <h3>{glasses.name}</h3>
-              <p>{glasses.desc}</p>
-            </div>
-          </div>
-        );
-      })}
+        <div className="overlay-text">
+          <h3>{product.name}</h3>
+          <p>{product.desc}</p>
+        </div>
+      </div>
     </div>
   );
 }
